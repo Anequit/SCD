3258751902bfb0cab0ca8744b386f631bf419d7b
Bailey <Anequit@gmail.com>
2022-01-28 19:07:03 -0500

    Added Avalonia Project

---
+++ src/CyberdropDownloader.Avalonia/App.axaml.cs
@@ -0,0 +1,27 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using CyberdropDownloader.Avalonia.ViewModels;
+using CyberdropDownloader.Avalonia.Views;
+
+namespace CyberdropDownloader.Avalonia;
+public class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainWindowViewModel(),
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ src/CyberdropDownloader.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberdropDownloader.Avalonia.ViewModels;
+public class MainWindowViewModel : ReactiveObject
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
