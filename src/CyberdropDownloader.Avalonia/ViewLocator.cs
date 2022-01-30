a21b167f68ede1292534cd9fb7f679255be27479
Bailey <Anequit@gmail.com>
2022-01-30 02:40:42 -0500

    Code cleanup

--- src/CyberdropDownloader.Avalonia/ViewLocator.cs
+++ src/CyberdropDownloader.Avalonia/ViewLocator.cs
@@ -4,6 +4,7 @@ using ReactiveUI;
 using System;
 
 namespace CyberdropDownloader.Avalonia;
+
 public class ViewLocator : IDataTemplate
 {
     public IControl Build(object data)
@@ -11,7 +12,7 @@ public class ViewLocator : IDataTemplate
         string? name = data.GetType().FullName!.Replace("ViewModel", "View");
         Type? type = Type.GetType(name);
 
-        if (type != null)
+        if(type != null)
         {
             return (Control)Activator.CreateInstance(type)!;
         }
