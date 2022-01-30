35e2c350ddee7bd7974923328c369cc194f7a112
Bailey <Anequit@gmail.com>
2022-01-30 02:47:22 -0500

    MainFormView

--- src/CyberdropDownloader.Avalonia/Views/MainFormView.axaml.cs
+++ src/CyberdropDownloader.Avalonia/Views/MainFormView.axaml.cs
@@ -1,11 +1,17 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace CyberdropDownloader.Avalonia.Views;
-
 public partial class MainFormView : UserControl
 {
-    public MainFormView() => InitializeComponent();
+    public MainFormView()
+    {
+        InitializeComponent();
+    }
 
-    private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
 }
