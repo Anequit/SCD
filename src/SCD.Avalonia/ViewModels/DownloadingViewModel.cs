6877f50c4859bec1a64020981e0241779f8dc2fb
Bailey <Anequit@gmail.com>
2022-02-01 05:43:00 -0500

    ViewModels

---
+++ src/SCD.Avalonia/ViewModels/DownloadingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCD.Avalonia.ViewModels;
+
+public class DownloadingViewModel : ReactiveObject
+{
+}
