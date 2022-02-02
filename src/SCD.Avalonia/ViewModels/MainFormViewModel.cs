350f202bfc3584a17a89977bd0fc04ded948096e
Bailey <Anequit@gmail.com>
2022-02-01 21:36:53 -0500

    Update MainFormViewModel.cs

--- src/SCD.Avalonia/ViewModels/MainFormViewModel.cs
+++ src/SCD.Avalonia/ViewModels/MainFormViewModel.cs
@@ -1,12 +1,34 @@
 ﻿using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SCD.Avalonia.Services;
+using System.Reactive;
 
 namespace SCD.Avalonia.ViewModels;
 
 public class MainFormViewModel : ReactiveObject
 {
+    private string _albumURL = string.Empty;
+    private string _downloadLocation = string.Empty;
+
+    public MainFormViewModel(Navigator navigator)
+    {
+        ReportBugCommand = ReactiveCommand.Create(() => { });
+        DownloadCommand = ReactiveCommand.Create(() => { });
+        SelectCommand = ReactiveCommand.Create(() => { });
+    }
+
+    public string AlbumURL
+    {
+        get => _albumURL;
+        set => this.RaiseAndSetIfChanged(ref _albumURL, value);
+    }
+
+    public string DownloadLocation
+    {
+        get => _downloadLocation;
+        set => this.RaiseAndSetIfChanged(ref _downloadLocation, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> ReportBugCommand { get; }
+    public ReactiveCommand<Unit, Unit> DownloadCommand { get; }
+    public ReactiveCommand<Unit, Unit> SelectCommand { get; }
 }
