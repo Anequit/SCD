6e45e5898f92b7f4821a959ecba6e99dd31e5279
Bailey <Anequit@gmail.com>
2022-02-02 21:57:11 -0500

    Exceptions

---
+++ src/SCD.Core/Exceptions/InvalidPathException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace SCD.Core.Exceptions;
+
+public class InvalidPathException : Exception
+{
+    public InvalidPathException()
+    {
+    }
+
+    public InvalidPathException(string? message) : base(message)
+    {
+    }
+
+    public InvalidPathException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected InvalidPathException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
