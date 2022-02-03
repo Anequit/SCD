ebae9bceb303f17625bc02d1e9414a25d0d0b4df
Bailey <Anequit@gmail.com>
2022-02-02 21:57:18 -0500

    DataModels

---
+++ src/SCD.Core/DataModels/Album.cs
@@ -0,0 +1,7 @@
+﻿namespace SCD.Core.DataModels;
+
+public class Album
+{
+    public bool Success { get; set; } = false;
+    public AlbumFile[]? Files { get; set; }
+}
---
+++ src/SCD.Core/DataModels/AlbumFile.cs
@@ -0,0 +1,12 @@
+﻿namespace SCD.Core.DataModels;
+
+public class AlbumFile
+{
+    public string Name { get; set; } = "";
+    public string File { get; set; } = "";
+
+    public void Download()
+    {
+
+    }
+}
