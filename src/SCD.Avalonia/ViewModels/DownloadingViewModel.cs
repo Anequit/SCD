d2a489af0e0197aa241d41b916c4a8e5b4667871
Bailey <Anequit@gmail.com>
2022-02-02 21:57:51 -0500

    Update DownloadingViewModel.cs

--- src/SCD.Avalonia/ViewModels/DownloadingViewModel.cs
+++ src/SCD.Avalonia/ViewModels/DownloadingViewModel.cs
@@ -1,12 +1,46 @@
-﻿using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using SCD.Avalonia.Services;
+using SCD.Core;
+using SCD.Core.DataModels;
+using System.Reactive;
 
 namespace SCD.Avalonia.ViewModels;
 
 public class DownloadingViewModel : ReactiveObject
 {
+    private Navigator _navigator;
+    private Window _window;
+
+    private int _progress = 0;
+    private string _filename = "";
+
+    public DownloadingViewModel(Navigator navigator, Window window, Album album)
+    {
+        _navigator = navigator;
+        _window = window;
+
+        CancelDownloadCommand = ReactiveCommand.Create(() => CancelDownload());
+    }
+
+    public string Filename
+    {
+        get => _filename;
+        set => this.RaiseAndSetIfChanged(ref _filename, value);
+    }
+
+    public int Progress
+    {
+        get => _progress;
+        set => this.RaiseAndSetIfChanged(ref _progress, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> CancelDownloadCommand { get; }
+
+    private void CancelDownload()
+    {
+        HttpClientHandler.Cancel();
+
+        _navigator.CurrentViewModel = new MainFormViewModel(_navigator, _window);
+    }
 }
