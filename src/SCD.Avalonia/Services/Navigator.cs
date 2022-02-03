bd4a3dca22cd0bd191a3ba174726b9cd95338b46
Bailey <Anequit@gmail.com>
2022-02-02 21:57:55 -0500

    Update Navigator.cs

--- src/SCD.Avalonia/Services/Navigator.cs
+++ src/SCD.Avalonia/Services/Navigator.cs
@@ -1,4 +1,5 @@
 ﻿using ReactiveUI;
+using SCD.Avalonia.ViewModels;
 using System;
 using System.Collections.ObjectModel;
 
@@ -6,27 +7,54 @@ namespace SCD.Avalonia.Services;
 
 public class Navigator
 {
-    private readonly ObservableCollection<ReactiveObject> _viewModels;
+    private readonly ObservableCollection<ReactiveObject?> _viewModels;
 
     public event Action? CurrentViewModelChanged;
+    public event Action? CurrentAlertViewModelChanged;
 
-    public Navigator() => _viewModels = new ObservableCollection<ReactiveObject>();
+    public Navigator() => _viewModels = new ObservableCollection<ReactiveObject?>();
 
-    public ReactiveObject CurrentViewModel
+    public ObservableCollection<ReactiveObject?> ViewModels => _viewModels;
+
+    public ReactiveObject? CurrentViewModel
     {
-        get => _viewModels[_viewModels.Count - 1];
+        get => _viewModels[0];
         set
         {
-            PreviousViewModel = CurrentViewModel;
+            if(_viewModels.Count == 0)
+            {
+                _viewModels.Add(value);
+            }
 
-            _viewModels[_viewModels.Count - 1] = value;
+            _viewModels[0] = value;
             OnCurrentViewModelChanged();
         }
     }
 
-    public ObservableCollection<ReactiveObject> ViewModels => _viewModels;
+    public ReactiveObject? CurrentAlertViewModel
+    {
+        get
+        {
+            if(_viewModels[_viewModels.Count - 1] is not AlertViewModel)
+            {
+                return null;
+            }
 
-    public ReactiveObject? PreviousViewModel { get; private set; }
+            return _viewModels[_viewModels.Count - 1];
+        }
+        set
+        {
+            if(_viewModels.Count == 1)
+            {
+                _viewModels.Add(value);
+            }
+
+            _viewModels[_viewModels.Count - 1] = value;
+
+            OnCurrentAlertViewModelChanged();
+        }
+    }
 
     private void OnCurrentViewModelChanged() => CurrentViewModelChanged?.Invoke();
+    private void OnCurrentAlertViewModelChanged() => CurrentAlertViewModelChanged?.Invoke();
 }
