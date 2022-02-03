fb0e17db99dce909617d4aa43982acbb3259702b
Bailey <Anequit@gmail.com>
2022-02-02 21:57:53 -0500

    Create AlertViewModel.cs

---
+++ src/SCD.Avalonia/ViewModels/AlertViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveUI;
+using SCD.Avalonia.Services;
+using System.Reactive;
+
+namespace SCD.Avalonia.ViewModels;
+
+public class AlertViewModel : ReactiveObject
+{
+    private readonly Navigator _navigator;
+
+    public AlertViewModel(Navigator navigator, string error, string errorMessage)
+    {
+        _navigator = navigator;
+
+        Error = error;
+        ErrorMessage = errorMessage;
+
+        CloseCommand = ReactiveCommand.Create(() => Close());
+    }
+
+    public string Error { get; }
+    public string ErrorMessage { get; }
+
+    public ReactiveCommand<Unit, Unit> CloseCommand { get; }
+
+    private void Close() => _navigator.CurrentAlertViewModel = null;
+}
