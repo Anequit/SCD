4157ce818c26426a6bbb146d2f1737b836a4998c
Bailey <Anequit@gmail.com>
2022-02-02 21:56:59 -0500

    Create HttpClientHandler.cs

---
+++ src/SCD.Core/HttpClientHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace SCD.Core;
+
+public static class HttpClientHandler
+{
+    private static HttpClient? _httpClient;
+
+    public static HttpClient HttpClient
+    {
+        get
+        {
+            if(_httpClient == null)
+            {
+                _httpClient = new HttpClient(new SocketsHttpHandler()
+                {
+                    AllowAutoRedirect = true,
+                    KeepAlivePingTimeout = Timeout.InfiniteTimeSpan
+                });
+            }
+
+            return _httpClient;
+        }
+    }
+
+    public static void Cancel() => HttpClient.CancelPendingRequests();
+}
