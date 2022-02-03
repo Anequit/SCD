857dba837b89a97ddd5ea5a3189af1421345709c
Bailey <Anequit@gmail.com>
2022-02-02 21:57:44 -0500

    Update TitleBarViewModel.cs

--- src/SCD.Avalonia/ViewModels/TitleBarViewModel.cs
+++ src/SCD.Avalonia/ViewModels/TitleBarViewModel.cs
@@ -1,7 +1,25 @@
-﻿using ReactiveUI;
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System.Reactive;
 
 namespace SCD.Avalonia.ViewModels;
 
 public class TitleBarViewModel : ReactiveObject
 {
+    private readonly Window _window;
+
+    public TitleBarViewModel(Window window)
+    {
+        _window = window;
+
+        MinimizeCommand = ReactiveCommand.Create(() => Minimize());
+        ExitCommand = ReactiveCommand.Create(() => Exit());
+    }
+
+    public ReactiveCommand<Unit, Unit> MinimizeCommand { get; }
+    public ReactiveCommand<Unit, Unit> ExitCommand { get; }
+
+    private void Minimize() => _window.WindowState = WindowState.Minimized;
+
+    private void Exit() => _window.Close();
 }
