273557c66a4129f95d382931feba644b9a033d12
Bailey <Anequit@gmail.com>
2022-02-04 16:32:09 -0500

    Update AlbumFile.cs

--- src/SCD.Core/DataModels/AlbumFile.cs
+++ src/SCD.Core/DataModels/AlbumFile.cs
@@ -2,11 +2,6 @@
 
 public class AlbumFile
 {
-    public string Name { get; set; } = "";
-    public string File { get; set; } = "";
-
-    public void Download()
-    {
-
-    }
+    public string Name { get; set; } = "Unknown filename";
+    public string? File { get; set; }
 }
