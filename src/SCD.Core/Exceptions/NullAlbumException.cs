f51642dec29075d74f57eda6e86f97100ef2c80e
Bailey <Anequit@gmail.com>
2022-02-04 15:14:15 -0500

    Added exceptions

---
+++ src/SCD.Core/Exceptions/NullAlbumException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace SCD.Core.Exceptions;
+
+public class NullAlbumException : Exception
+{
+    public NullAlbumException()
+    {
+    }
+
+    public NullAlbumException(string? message) : base(message)
+    {
+    }
+
+    public NullAlbumException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected NullAlbumException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
