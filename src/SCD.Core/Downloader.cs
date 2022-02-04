80a0f1f4744654fdeb72861b062ef6b71395bec2
Bailey <Anequit@gmail.com>
2022-02-04 16:32:08 -0500

    Create Downloader.cs

---
+++ src/SCD.Core/Downloader.cs
@@ -0,0 +1,20 @@
+﻿using SCD.Core.DataModels;
+
+namespace SCD.Core;
+
+public static class Downloader
+{
+    public static event EventHandler<AlbumFile>? FileChanged;
+    public static event EventHandler<int>? ProgressChanged;
+
+    public static void DownloadAlbum(Album album)
+    {
+        if(album.Files is null || album.Files.Length == 0)
+            return;
+
+        foreach(AlbumFile file in album.Files)
+        {
+            // Download logic for each file
+        }
+    }
+}
