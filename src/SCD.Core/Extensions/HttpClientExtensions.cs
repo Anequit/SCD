c9b395eb12c7ddd289166769d672472f4ab7cefe
Bailey <Anequit@gmail.com>
2022-02-05 17:21:07 -0500

    Added extensions to HttpClient and Stream

---
+++ src/SCD.Core/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace SCD.Core.Extensions;
+
+public static class HttpClientExtensions
+{
+    public static async Task DownloadAsync(this HttpClient httpClient, string url, Stream destination, IProgress<decimal> progress, CancellationToken cancellationToken)
+    {
+        using(HttpResponseMessage? response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+        {
+            long? contentLength = response.Content.Headers.ContentLength;
+
+            using(Stream? content = await response.Content.ReadAsStreamAsync(cancellationToken))
+            {
+                if(progress == null || !contentLength.HasValue)
+                {
+                    await content.CopyToAsync(destination);
+                    return;
+                }
+
+                Progress<long>? relativeProgress = new Progress<long>(totalBytes => progress.Report((decimal)totalBytes / contentLength.Value * 100));
+
+                await content.CopyToAsync(destination, 8192, relativeProgress, cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/SCD.Core/Extensions/StreamExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace SCD.Core.Extensions;
+
+public static class StreamExtensions
+{
+    public static async Task CopyToAsync(this Stream source, Stream stream, int bufferSize, IProgress<long> progress, CancellationToken cancellationToken)
+    {
+        int bytesRead;
+        byte[]? buffer = new byte[bufferSize];
+        long totalBytesRead = 0;
+
+        do
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false);
+
+            await stream.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+
+            totalBytesRead += bytesRead;
+            progress?.Report(totalBytesRead);
+        } while(bytesRead != 0);
+    }
+}
