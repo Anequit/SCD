b3ffc91af88bdcb42ce5d0680e0c90090724e06e
Bailey <Anequit@gmail.com>
2022-02-05 17:21:18 -0500

    Update Album.cs

--- src/SCD.Core/DataModels/Album.cs
+++ src/SCD.Core/DataModels/Album.cs
@@ -3,6 +3,7 @@
 public class Album
 {
     public bool Success { get; set; } = false;
+    public string? Title { get; set; }
     public string? Description { get; set; }
     public AlbumFile[]? Files { get; set; }
 }
