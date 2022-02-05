1951e202d1936bb1c272ba045caaf355b783f875
Bailey <Anequit@gmail.com>
2022-02-05 17:20:37 -0500

    Create PathUtilities.cs

---
+++ src/SCD.Core/Utilities/PathUtilities.cs
@@ -0,0 +1,14 @@
+﻿namespace SCD.Core.Utilities;
+
+public static class PathUtilities
+{
+    public static string NormalizePath(string path)
+    {
+        string invalidChars = new string(Path.GetInvalidPathChars());
+
+        foreach(char invalidChar in invalidChars)
+            path.Replace($"{invalidChar}", string.Empty);
+
+        return path;
+    }
+}
