64bf1be6f6fa1edd308456fcf357ad8a65e4e35a
Bailey <Anequit@gmail.com>
2022-02-06 05:15:53 -0500

    Update PathUtilities.cs

--- src/SCD.Core/Utilities/PathUtilities.cs
+++ src/SCD.Core/Utilities/PathUtilities.cs
@@ -1,7 +1,26 @@
-﻿namespace SCD.Core.Utilities;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace SCD.Core.Utilities;
 
 public static class PathUtilities
 {
+    public static void Open(string path)
+    {
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Process.Start("explorer", path);
+        }
+        else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("xdg-open", path);
+        }
+        else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", path);
+        }
+    }
+
     public static string NormalizePath(string path)
     {
         string invalidChars = new string(Path.GetInvalidPathChars());
