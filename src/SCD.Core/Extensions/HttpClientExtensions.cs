56ae869023bea4fd712bf6afff9a60283107c6a6
Bailey <Anequit@gmail.com>
2022-02-08 20:37:07 -0500

    Implemented multi threading

--- src/SCD.Core/Extensions/HttpClientExtensions.cs
+++ src/SCD.Core/Extensions/HttpClientExtensions.cs
@@ -8,30 +8,33 @@ public static class HttpClientExtensions
     {
         using(HttpResponseMessage response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
         {
-            // If content length is null, then set contentLength to default 500mb
             long contentLength = (long)(response.Content.Headers.ContentLength is not null ? response.Content.Headers.ContentLength : 500000000);
-            long dataDownloaded = 0;
             long buffer = (contentLength < 100_000) ? (contentLength / 100) : 100_000;
+            long dataDownloaded = 0;
 
-            do
+            List<int> startingHeaderRangeValues = httpClient.GetStartingHeaderRangeValues(contentLength, buffer);
+
+            await Parallel.ForEachAsync(startingHeaderRangeValues, cancellationToken, async (i, cancellationToken) =>
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
                 using(HttpRequestMessage httpRequest = new HttpRequestMessage())
                 {
                     httpRequest.RequestUri = new Uri(url);
-                    httpRequest.Headers.Range = new RangeHeaderValue(dataDownloaded, dataDownloaded + buffer);
+                    httpRequest.Headers.Range = new RangeHeaderValue(i, i + buffer);
 
                     using(HttpResponseMessage httpResponse = await HttpClientHelper.HttpClient.SendAsync(httpRequest, cancellationToken))
                     {
-                        destination.Seek(dataDownloaded, SeekOrigin.Begin);
+                        destination.Seek(i, SeekOrigin.Begin);
                         await destination.WriteAsync(await httpResponse.Content.ReadAsByteArrayAsync(), cancellationToken);
 
                         dataDownloaded += (long)(httpResponse.Content.Headers.ContentLength is not null ? httpResponse.Content.Headers.ContentLength : 0);
                         progress.Report((decimal)dataDownloaded / contentLength * 100);
                     }
                 }
-            } while(dataDownloaded != response.Content.Headers.ContentLength);
+            });
         }
     }
+
+    public static List<int> GetStartingHeaderRangeValues(this HttpClient httpClient, long contentLength, long bufferSize) => Enumerable.Range(0, (int)contentLength).Where(x => x % bufferSize == 0).ToList();
 }
