a4ba24ba79b7b1de3b85fc823087434af45a8d2a
Bailey <Anequit@gmail.com>
2022-02-11 17:47:44 -0500

    Update HttpClientExtensions.cs

--- src/SCD.Core/Extensions/HttpClientExtensions.cs
+++ src/SCD.Core/Extensions/HttpClientExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Headers;
+﻿using SCD.Core.DataModels;
+using System.Collections.Concurrent;
+using System.Net.Http.Headers;
 
 namespace SCD.Core.Extensions;
 
@@ -12,29 +14,66 @@ public static class HttpClientExtensions
             long buffer = (contentLength < 100_000) ? (contentLength / 100) : 100_000;
             long dataDownloaded = 0;
 
-            List<int> startingHeaderRangeValues = httpClient.GetStartingHeaderRangeValues(contentLength, buffer);
+            List<FileChunk> fileChunks = GetFileChunks(contentLength, buffer);
 
-            await Parallel.ForEachAsync(startingHeaderRangeValues, cancellationToken, async (i, cancellationToken) =>
+            Parallel.ForEach(Partitioner.Create(fileChunks, true), async i =>
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
-                using(HttpRequestMessage httpRequest = new HttpRequestMessage())
+                using(HttpRequestMessage httpRequestMessage = new HttpRequestMessage())
                 {
-                    httpRequest.RequestUri = new Uri(url);
-                    httpRequest.Headers.Range = new RangeHeaderValue(i, i + buffer);
+                    httpRequestMessage.RequestUri = new Uri(url);
+                    httpRequestMessage.Headers.Range = new RangeHeaderValue(i.StartingHeaderRange, i.EndingHeaderRange);
 
-                    using(HttpResponseMessage httpResponse = await HttpClientHelper.HttpClient.SendAsync(httpRequest, cancellationToken))
+                    HttpResponseMessage? httpResponseMessage = null;
+
+                    do
                     {
-                        destination.Seek(i, SeekOrigin.Begin);
-                        await destination.WriteAsync(await httpResponse.Content.ReadAsByteArrayAsync(), cancellationToken);
+                        try
+                        {
+                            httpResponseMessage = HttpClientHelper.HttpClient.SendAsync(httpRequestMessage, cancellationToken).Result;
+                        }
+                        finally
+                        {
+                            if(httpResponseMessage is not null)
+                            {
+                                i.Data = await httpResponseMessage.Content.ReadAsByteArrayAsync(cancellationToken);
+                                i.Downloaded = true;
 
-                        dataDownloaded += (long)(httpResponse.Content.Headers.ContentLength is not null ? httpResponse.Content.Headers.ContentLength : 0);
-                        progress.Report((decimal)dataDownloaded / contentLength * 100);
-                    }
+                                dataDownloaded += (long)(httpResponseMessage.Content.Headers.ContentLength is not null ? httpResponseMessage.Content.Headers.ContentLength : 0);
+                                progress.Report((decimal)dataDownloaded / contentLength * 100);
+
+                                httpResponseMessage.Dispose();
+                            }
+                        }
+                    } while(!i.Downloaded);
                 }
             });
+
+            foreach(FileChunk chunk in fileChunks)
+            {
+                destination.Seek(chunk.StartingHeaderRange, SeekOrigin.Begin);
+                await destination.WriteAsync(chunk.Data, cancellationToken);
+            }
         }
     }
 
-    public static List<int> GetStartingHeaderRangeValues(this HttpClient httpClient, long contentLength, long bufferSize) => Enumerable.Range(0, (int)contentLength).Where(x => x % bufferSize == 0).ToList();
+    private static List<FileChunk> GetFileChunks(long contentLength, long bufferSize)
+    {
+        List<FileChunk> fileChunks = new List<FileChunk>();
+
+        List<int> startingHeaderRanges = Enumerable.Range(0, (int)contentLength).Where(x => x % bufferSize == 0).ToList();
+
+        foreach(int i in Enumerable.Range(0, startingHeaderRanges.Count))
+        {
+            fileChunks.Add(new FileChunk()
+            {
+                Position = i,
+                StartingHeaderRange = startingHeaderRanges[i],
+                EndingHeaderRange = startingHeaderRanges[i] + (int)bufferSize
+            });
+        }
+
+        return fileChunks;
+    }
 }
