412998f3f81ce6385a0fb7a09ecbc403d847a4ca
Bailey <Anequit@gmail.com>
2022-02-12 16:49:43 -0500

    Create NullContentLengthException.cs

---
+++ src/SCD.Core/Exceptions/NullContentLengthException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace SCD.Core.Exceptions;
+
+public class NullContentLengthException : Exception
+{
+    public NullContentLengthException()
+    {
+    }
+
+    public NullContentLengthException(string? message) : base(message)
+    {
+    }
+
+    public NullContentLengthException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected NullContentLengthException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
