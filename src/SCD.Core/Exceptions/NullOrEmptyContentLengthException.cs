a0ac17465c7f819a2039ffa38ff3d8b54e3fe2c5
Bailey <Anequit@gmail.com>
2022-02-18 08:49:32 -0500

    Renamed NullContentLengthException

---
+++ src/SCD.Core/Exceptions/NullOrEmptyContentLengthException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SCD.Core.Exceptions;
+
+public class NullOrEmptyContentLengthException : Exception
+{
+    public NullOrEmptyContentLengthException()
+    {
+    }
+
+    public NullOrEmptyContentLengthException(string? message) : base(message)
+    {
+    }
+
+    public NullOrEmptyContentLengthException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected NullOrEmptyContentLengthException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
