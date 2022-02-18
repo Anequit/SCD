233792476c7a8ae23b93eab4003f60d5acec73a5
Bailey <Anequit@gmail.com>
2022-02-18 04:08:38 -0500

    Update FileChunk.cs

--- src/SCD.Core/DataModels/FileChunk.cs
+++ src/SCD.Core/DataModels/FileChunk.cs
@@ -2,11 +2,11 @@
 
 public class FileChunk
 {
-    public int Position { get; set; } = 0;
+    public int Position { get; init; }
 
-    public long StartingHeaderRange { get; set; } = 0;
-    public long EndingHeaderRange { get; set; } = 0;
+    public long StartingHeaderRange { get; init; }
+    public long EndingHeaderRange { get; init; }
 
-    public bool Downloaded { get; set; } = false;
+    public bool Downloaded { get; set; }
     public byte[]? Data { get; set; }
 }
