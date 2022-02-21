f864ed5fc9ac8f4d87565df8383c5ca76263d59a
Bailey <Anequit@gmail.com>
2022-02-21 00:10:19 -0500

    Create FileUtilities.cs

---
+++ src/SCD.Core/Utilities/FileUtilities.cs
@@ -0,0 +1,20 @@
+﻿using SCD.Core.DataModels;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SCD.Core.Utilities;
+
+public static class FileUtilities
+{
+    public static async Task Save(FileChunk[] fileChunks, Stream destination, CancellationToken cancellationToken)
+    {
+        foreach(FileChunk chunk in fileChunks)
+        {
+            // Seek to the correct position, write the chunks data, and then flush the fileStream
+            destination.Seek(chunk.StartingHeaderRange, SeekOrigin.Begin);
+            await destination.WriteAsync(chunk.Data, cancellationToken);
+            await destination.FlushAsync(cancellationToken);
+        }
+    }
+}
