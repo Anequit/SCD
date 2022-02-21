abee87524ffa89c7d79a3ffdc255b1427bcbfb0c
Bailey <Anequit@gmail.com>
2022-02-21 00:10:55 -0500

    Create Release.cs

---
+++ src/SCD.Core/DataModels/Release.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SCD.Core.DataModels;
+
+public class Release
+{
+    [JsonPropertyName("tag_name")]
+    public string? VersionNumber { get; set; }
+}
