b1b07712e2f111aff441e02b9ffb01ce14dde655
Bailey <Anequit@gmail.com>
2022-02-22 20:13:00 -0500

    Added ErrorAlertView and it's model

---
+++ src/SCD.Avalonia/ViewModels/ErrorAlertViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using SCD.Avalonia.Services;
+using System.Reactive;
+
+namespace SCD.Avalonia.ViewModels;
+
+public class ErrorAlertViewModel : ReactiveObject
+{
+    public ErrorAlertViewModel(string error, string errorMessage)
+    {
+        Error = error;
+        ErrorMessage = errorMessage;
+
+        CloseCommand = ReactiveCommand.Create(() => Close());
+    }
+
+    public string Error { get; }
+    public string ErrorMessage { get; }
+
+    public ReactiveCommand<Unit, Unit> CloseCommand { get; }
+
+    private void Close() => NavigationService.CloseAlert();
+}
