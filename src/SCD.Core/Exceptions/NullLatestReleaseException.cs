7ddfae39611e7f09bc3d9b833884cbf94f3b43ba
Bailey <Anequit@gmail.com>
2022-02-22 20:06:10 -0500

    Revert "Removed redundant exceptions"
    This reverts commit 8c3bd5954f54472af9ef2f87e0ef623a82d181c9.

---
+++ src/SCD.Core/Exceptions/NullLatestReleaseException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SCD.Core.Exceptions;
+
+internal class NullReleaseException : Exception
+{
+    public NullReleaseException()
+    {
+    }
+
+    public NullReleaseException(string? message) : base(message)
+    {
+    }
+
+    public NullReleaseException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected NullReleaseException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ src/SCD.Core/Exceptions/UnsuccessfulAlbumException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SCD.Core.Exceptions;
+
+public class UnsuccessfulAlbumException : Exception
+{
+    public UnsuccessfulAlbumException()
+    {
+    }
+
+    public UnsuccessfulAlbumException(string? message) : base(message)
+    {
+    }
+
+    public UnsuccessfulAlbumException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected UnsuccessfulAlbumException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
