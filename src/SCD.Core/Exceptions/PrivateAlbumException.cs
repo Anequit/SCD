8345d533d8fcb0297e9efe4cf96da9bfac366caf
Bailey <Anequit@gmail.com>
2022-02-22 19:47:28 -0500

    Create PrivateAlbumException.cs

---
+++ src/SCD.Core/Exceptions/PrivateAlbumException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SCD.Core.Exceptions;
+
+public class PrivateAlbumException : Exception
+{
+    public PrivateAlbumException()
+    {
+    }
+
+    public PrivateAlbumException(string? message) : base(message)
+    {
+    }
+
+    public PrivateAlbumException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected PrivateAlbumException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
