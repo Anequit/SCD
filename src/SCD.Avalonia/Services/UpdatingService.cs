4afe8a2b51a223a3a94a6f41163cafd0283f7ccc
Bailey <Anequit@gmail.com>
2022-02-22 20:51:31 -0500

    Refactored UpdateService

--- src/SCD.Avalonia/Services/UpdatingService.cs
+++ src/SCD.Avalonia/Services/UpdatingService.cs
@@ -10,24 +10,50 @@ public static class UpdateService
     /// <summary>
     /// Current version of the application.
     /// </summary>
-    public static Version? CurrentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+    public static Version? CurrentVersion { get; } = Assembly.GetExecutingAssembly().GetName().Version;
 
     /// <summary>
     /// Checks if there is a newer version on github.
     /// </summary>
     /// <returns>True if update is available.</returns>
-    public static bool CheckForUpdateAsync()
+    public static async Task CheckForUpdateAsync()
     {
-        bool needsUpdated = false;
-
-        Task.Run(async () =>
+        try
         {
-            Version latestVersion = new Version(await WebUtilities.FetchLatestVersion());
+            if(CurrentVersion is null)
+                return;
 
-            if(CurrentVersion < latestVersion)
-                needsUpdated = true;
-        });
+            Release latestRelease = await WebUtilities.FetchLatestRelease();
 
-        return needsUpdated;
+            Version latestVersion = new Version(latestRelease.VersionNumber.Remove(0, 1));
+
+            // Normalizes the version to match how latestVersion is parsed
+            // Without normalization it wouldn't be able to tell if there was a new version
+            Version currentVersion = new Version(CurrentVersion.ToString(3));
+
+            if(latestVersion > currentVersion)
+                NavigationService.ShowUpdateAlert("Update Available", "Would you like to update?", latestRelease);
+        }
+        catch(Exception exception)
+        {
+            switch(exception)
+            {
+                case NullReleaseException:
+                    NavigationService.ShowErrorAlert("Error", "Failed to get latest release.");
+                    break;
+
+                case NullOrEmptyVersionNumberException:
+                    NavigationService.ShowErrorAlert("Error", "Failed to get latest version number.");
+                    break;
+
+                case NullOrEmptyUrlException:
+                    NavigationService.ShowErrorAlert("Error", "Failed to get latest version url.");
+                    break;
+
+                default:
+                    NavigationService.ShowErrorAlert("Unknown Error", exception.Message);
+                    break;
+            }
+        }
     }
 }
