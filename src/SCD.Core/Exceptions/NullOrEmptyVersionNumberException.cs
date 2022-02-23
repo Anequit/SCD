da459b776501e4a9a36b2470b6ab16e7ab08e4df
Bailey <Anequit@gmail.com>
2022-02-22 19:47:25 -0500

    Modified two exceptions

--- src/SCD.Core/Exceptions/NullOrEmptyVersionNumberException.cs
+++ src/SCD.Core/Exceptions/NullOrEmptyVersionNumberException.cs
@@ -3,7 +3,7 @@ using System.Runtime.Serialization;
 
 namespace SCD.Core.Exceptions;
 
-internal class NullOrEmptyVersionNumberException : Exception
+public class NullOrEmptyVersionNumberException : Exception
 {
     public NullOrEmptyVersionNumberException()
     {
