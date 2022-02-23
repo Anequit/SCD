4ca56379b5d20ce4a96273a3e1d9f1c30d6af17d
Bailey <Anequit@gmail.com>
2022-02-22 19:48:03 -0500

    Update Release.cs

--- src/SCD.Core/DataModels/Release.cs
+++ src/SCD.Core/DataModels/Release.cs
@@ -5,5 +5,8 @@ namespace SCD.Core.DataModels;
 public class Release
 {
     [JsonPropertyName("tag_name")]
-    public string? VersionNumber { get; set; }
+    public string VersionNumber { get; set; } = "";
+
+    [JsonPropertyName("html_url")]
+    public string Url { get; set; } = "";
 }
