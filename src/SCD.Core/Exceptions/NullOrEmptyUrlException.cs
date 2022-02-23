450a86926f3e15108212a156d76d097089155d70
Bailey <Anequit@gmail.com>
2022-02-22 20:07:21 -0500

    Revert "Revert "Added exceptions""
    This reverts commit d69f49771cea43fa7f93fe6fcc1c53b6c0b1a6f4.

---
+++ src/SCD.Core/Exceptions/NullOrEmptyUrlException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SCD.Core.Exceptions;
+
+public class NullOrEmptyUrlException : Exception
+{
+    public NullOrEmptyUrlException()
+    {
+    }
+
+    public NullOrEmptyUrlException(string? message) : base(message)
+    {
+    }
+
+    public NullOrEmptyUrlException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected NullOrEmptyUrlException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
