6299b5c785ad6094f9dfe4488e49192bcc77497b
Bailey <Anequit@gmail.com>
2022-02-22 19:48:00 -0500

    Update Album.cs

--- src/SCD.Core/DataModels/Album.cs
+++ src/SCD.Core/DataModels/Album.cs
@@ -1,9 +1,18 @@
-﻿namespace SCD.Core.DataModels;
+﻿using System.Text.Json.Serialization;
+
+namespace SCD.Core.DataModels;
 
 public record Album
 {
+    [JsonPropertyName("success")]
     public bool Success { get; init; }
+
+    [JsonPropertyName("title")]
     public string? Title { get; set; }
+
+    [JsonPropertyName("description")]
     public string? Description { get; init; }
-    public AlbumFile[]? Files { get; init; }
+
+    [JsonPropertyName("files")]
+    public AlbumFile[]? AlbumFiles { get; init; }
 }
