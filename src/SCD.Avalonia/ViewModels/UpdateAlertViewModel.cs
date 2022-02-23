760f83a1d6c1546bd343f3c841c18380ade509e5
Bailey <Anequit@gmail.com>
2022-02-22 20:13:24 -0500

    Added UpdateAlertView and it's model

---
+++ src/SCD.Avalonia/ViewModels/UpdateAlertViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ReactiveUI;
+using SCD.Avalonia.Services;
+using SCD.Core.DataModels;
+using SCD.Core.Utilities;
+using System.Reactive;
+
+namespace SCD.Avalonia.ViewModels;
+
+public class UpdateAlertViewModel : ReactiveObject
+{
+    private readonly Release _release;
+
+    public UpdateAlertViewModel(string title, string message, Release release)
+    {
+        _release = release;
+
+        Title = title;
+        Message = message;
+
+        YesCommand = ReactiveCommand.Create(() => Yes());
+        NoCommand = ReactiveCommand.Create(() => No());
+    }
+
+    public string Title { get; }
+    public string Message { get; }
+
+    public ReactiveCommand<Unit, Unit> YesCommand { get; }
+    public ReactiveCommand<Unit, Unit> NoCommand { get; }
+
+    private void Yes() => WebUtilities.Open(_release.Url);
+
+    private void No() => NavigationService.CloseAlert();
+}
