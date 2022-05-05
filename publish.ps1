404e2f75a24fca21a12abba81f3d074ceb35ae74
Bailey <Anequit@gmail.com>
2022-05-05 09:43:18 -0700

    v2.0.1 (#18)
    * Fixed occasional files failing
    
    * Fixed publish script errors
    
    * Updated assembly version

--- publish.ps1
+++ publish.ps1
@@ -3,25 +3,25 @@ $PublishPath = ".\src\SCD.Avalonia\bin\Publish"
 mkdir -Force $PublishPath
 
 dotnet publish .\SCD.sln -c Release -p:PublishProfile=win-x64 -v q --nologo
-Compress-Archive -Path $PublishPath\win-x64 -DestinationPath $PublishPath\win-x64
+Compress-Archive -Force -Path $PublishPath\win-x64 -DestinationPath $PublishPath\win-x64
 Remove-Item -Path $PublishPath\win-x64 -Force -Recurse -ErrorAction SilentlyContinue
 
 dotnet publish .\SCD.sln -c Release -p:PublishProfile=win-x86 -v q --nologo
-Compress-Archive -Path $PublishPath\win-x86 -DestinationPath $PublishPath\win-x86
+Compress-Archive -Force -Path $PublishPath\win-x86 -DestinationPath $PublishPath\win-x86
 Remove-Item -Path $PublishPath\win-x86 -Force -Recurse -ErrorAction SilentlyContinue
 
 dotnet publish .\SCD.sln -c Release -p:PublishProfile=win-arm64 -v q --nologo
-Compress-Archive -Path $PublishPath\win-arm64 -DestinationPath $PublishPath\win-arm64
+Compress-Archive -Force -Path $PublishPath\win-arm64 -DestinationPath $PublishPath\win-arm64
 Remove-Item -Path $PublishPath\win-arm64 -Force -Recurse -ErrorAction SilentlyContinue
 
 dotnet publish .\SCD.sln -c Release -p:PublishProfile=win-arm -v q --nologo
-Compress-Archive -Path $PublishPath\win-arm -DestinationPath $PublishPath\win-arm
+Compress-Archive -Force -Path $PublishPath\win-arm -DestinationPath $PublishPath\win-arm
 Remove-Item -Path $PublishPath\win-arm -Force -Recurse -ErrorAction SilentlyContinue
 
 dotnet publish .\SCD.sln -c Release -p:PublishProfile=linux-x64 -v q --nologo
-Compress-Archive -Path $PublishPath\linux-x64 -DestinationPath $PublishPath\linux-x64
+Compress-Archive -Force -Path $PublishPath\linux-x64 -DestinationPath $PublishPath\linux-x64
 Remove-Item -Path $PublishPath\linux-x64 -Force -Recurse -ErrorAction SilentlyContinue
 
 dotnet publish .\SCD.sln -c Release -p:PublishProfile=osx-x64 -v q --nologo
-Compress-Archive -Path $PublishPath\osx-x64 -DestinationPath $PublishPath\osx-x64
+Compress-Archive -Force -Path $PublishPath\osx-x64 -DestinationPath $PublishPath\osx-x64
 Remove-Item -Path $PublishPath\osx-x64 -Force -Recurse -ErrorAction SilentlyContinue
