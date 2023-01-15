82bfbd12238358bb02066ec8d6063d1d272c6387
Bailey <Anequit@gmail.com>
2023-01-15 09:13:20 -0500

    Document AlbumDownloader.cs

--- src/SCD.Core/AlbumDownloader.cs
+++ src/SCD.Core/AlbumDownloader.cs
@@ -15,51 +15,67 @@ public static class AlbumDownloader
 
     public async static Task DownloadAlbumAsync(Album album, string downloadLocation, IProgress<decimal> progress, CancellationToken token)
     {
+        // Create file directory for album files
         if(!Directory.Exists(downloadLocation))
             Directory.CreateDirectory(downloadLocation);
 
+        // Return early if album is emptyy
         if(album.AlbumFiles.Count == 0)
             return;
 
-        FileDownloader downloadHandler = new FileDownloader(progress, 1024 * 256, 1000);
+        // Initialize file downloader
+        FileDownloader fileDownloader = new FileDownloader(progress, 1024 * 256, 1000);
 
         do
         {
+            // Take first file in queue
             AlbumFile file = album.AlbumFiles.Peek();
 
+            // Notify UI of file change
             FileChanged?.Invoke(file);
 
             // If the file url is empty or the file name is empty, then skip over the file
             if(string.IsNullOrEmpty(file.Filename) || string.IsNullOrEmpty(file.Url))
                 continue;
 
+            // Create file path
             string filePath = Path.Combine(downloadLocation, Parser.ParseValidFilename(file.Filename));
 
+            // Check if current file exists already
             if(File.Exists(filePath))
             {
+                // Remove file from queue
                 album.AlbumFiles.Dequeue();
 
+                // Move to next file
                 continue;
             }
 
             try
             {
-                await downloadHandler.DownloadFileAsync(file, filePath, token);
+                // Download current file
+                await fileDownloader.DownloadFileAsync(file, filePath, token);
             }
             catch(Exception ex)
             {
+                // Remove file if exception was thrown
                 File.Delete(filePath);
 
+                // Propagate token cancellation exception
                 token.ThrowIfCancellationRequested();
 
+                // Retry if IO exception or http request exception
                 if(ex is IOException or HttpRequestException)
                     continue;
 
+                // Notify UI if unexpected error
                 ErrorOccurred?.Invoke(ex);
 
+                // Propagate unexpected error
                 throw;
             }
 
+            // Remove completed file from queue
             album.AlbumFiles.Dequeue();
         } while(album.AlbumFiles.Count > 0);
     }
